6d8ff1d717ff54600d672bed836b2af13aaac3ea
Arik Hativa <arikhativa@gmail.com>
2022-06-15 19:19:38 +0300

    working on publish

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 
 import * as vscode from 'vscode';
 
-function get_file_name(editor: vscode.TextEditor): string {
+function getFileName(editor: vscode.TextEditor): string {
 	let arr = editor.document.fileName.split("/");
 	let fileName = arr[arr.length - 1];
 	let re = /\./gi; 
@@ -12,21 +12,21 @@ function get_file_name(editor: vscode.TextEditor): string {
 function enterText() {
     const editor = vscode.window.activeTextEditor;
     if (editor) {
-		let fileName = get_file_name(editor);
+		let fileName = getFileName(editor);
 		let startLine: vscode.TextLine;
 		let lastLine = editor.document.lineAt(editor.document.lineCount - 1);
 		
 		for (let i = 0; i < editor.document.lineCount; i++) {
 			let line = editor.document.lineAt(i);
-			if (line.text == "")
+			if (line.text === "")
 			{
 				startLine = line;
 				break ;
 			}
 		}
 		editor.edit(editBuilder => {
-			editBuilder.insert(startLine.range.end, `#ifndef ${fileName}\n# define ${fileName}`);
-			editBuilder.insert(lastLine.range.start, "\n\n#endif");
+			editBuilder.insert(startLine.range.end, `\n#ifndef ${fileName}\n# define ${fileName}`);
+			editBuilder.insert(lastLine.range.start, "\n#endif");
 		});
     }
 }
