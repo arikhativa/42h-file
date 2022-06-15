45b257e2301ed08e7c48705a92e687f9c34b880f
Arik Hativa <arikhativa@gmail.com>
2022-06-15 18:57:37 +0300

    done

--- src/extension.ts
+++ src/extension.ts
@@ -1,52 +1,42 @@
 
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+function get_file_name(editor: vscode.TextEditor): string {
+	let arr = editor.document.fileName.split("/");
+	let fileName = arr[arr.length - 1];
+	let re = /\./gi; 
 
-function tmp() {
-	
+	return (fileName.replace(re, "_").toLocaleUpperCase());
 }
 
-function enterText(text: string) {
+function enterText() {
     const editor = vscode.window.activeTextEditor;
     if (editor) {
-		let path = editor.document.fileName;
-		let arr = path.split("/");
-		let fileName = arr[arr.length - 1];
-		let re = /\./gi; 
-		let name = fileName.replace(re, "_").toLocaleUpperCase();
+		let fileName = get_file_name(editor);
+		let startLine: vscode.TextLine;
+		let lastLine = editor.document.lineAt(editor.document.lineCount - 1);
 		
 		for (let i = 0; i < editor.document.lineCount; i++) {
 			let line = editor.document.lineAt(i);
 			if (line.text == "")
 			{
-				editor.edit(editBuilder => {
-					editBuilder.insert(line.range.end, `#ifndef ${name}\n# define ${name}`);
-				});
+				startLine = line;
+				break ;
 			}
 		}
-		let lastLine = editor.document.lineAt(editor.document.lineCount - 1);
 		editor.edit(editBuilder => {
-			editBuilder.insert(lastLine.range.start, "\n#endif\n");
+			editBuilder.insert(startLine.range.end, `#ifndef ${fileName}\n# define ${fileName}`);
+			editBuilder.insert(lastLine.range.start, "\n#endif");
 		});
     }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('42header.helloWorld', () => {
-
-		enterText("ss")
-
-		vscode.window.showInformationMessage('Hello World from 42header!');
+	let disposable = vscode.commands.registerCommand('42.h.protect', () => {
+		enterText();
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
