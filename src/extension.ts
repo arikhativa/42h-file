0f0e434d3843fb3a4c8bdc9e100df41e16603a5c
Arik Hativa <arikhativa@gmail.com>
2022-06-15 17:06:36 +0300

    looks good have issue of some sort

--- src/extension.ts
+++ src/extension.ts
@@ -4,24 +4,32 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
+function tmp() {
+	
+}
+
 function enterText(text: string) {
     const editor = vscode.window.activeTextEditor;
     if (editor) {
 		let path = editor.document.fileName;
-
 		let arr = path.split("/");
-
 		let fileName = arr[arr.length - 1];
-
 		let re = /\./gi; 
-
 		let name = fileName.replace(re, "_").toLocaleUpperCase();
 		
-		console.log(name);
-
-        editor.edit(editBuilder => {
-            editBuilder.insert(editor.selection.active, text);
-        });
+		for (let i = 0; i < editor.document.lineCount; i++) {
+			let line = editor.document.lineAt(i);
+			if (line.text == "")
+			{
+				editor.edit(editBuilder => {
+					editBuilder.insert(line.range.end, `#ifndef ${name}\n# define ${name}`);
+				});
+			}
+		}
+		let lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+		editor.edit(editBuilder => {
+			editBuilder.insert(lastLine.range.start, "\n#endif\n");
+		});
     }
 }
 
